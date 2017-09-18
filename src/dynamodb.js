e9ec7cb44915a80bf4c1d7771d79d95a8406caed
Luke <luke.shih@neogeeks.net>
2017-09-19 00:44:06 +0800

    add empty value check for dynamodb

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,4 +1,6 @@
 let AWS = require('aws-sdk');
+let _ = require('lodash');
+
 AWS.config.update({
     region: "us-east-1"
 });
@@ -96,39 +98,66 @@ async function scanData(tableName){
     }
 }
 
+function fixEmptyValue(data){
+  let outputData = {};
+  for(let i in data){
+
+    if(data[i] === ""){
+      continue;
+    }
+    else if(Array.isArray(data[i])){
+      data[i] = data[i].filter(elem => {
+        return elem !== "";
+      });
+    }
+    else if(typeof data[i] == 'object'){
+      data[i] = fixEmptyValue(data[i]);
+    }
+
+    outputData[i] = data[i];
+  }
+
+  return outputData;
+}
+
 function postData(tableName, data){
-    var params = {
-        TableName: tableName,
-        Item: data
-    };
-    console.log("==postData==");
-    console.log(params.Item);
-    return new Promise((resolve, reject) => {
+  //check
+  let inputData = fixEmptyValue(data);
 
-        docClient.put(params).promise().then(result => {
-            console.log("Added item:", JSON.stringify(result, null, 2));
-            resolve(result);
-        }).catch(err => {
-            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-            reject(err);
-        });
+  var params = {
+      TableName: tableName,
+      Item: inputData
+  };
+  console.log("==postData==");
+  console.log(params.Item);
+  return new Promise((resolve, reject) => {
 
-    });
+      docClient.put(params).promise().then(result => {
+          console.log("Added item:", JSON.stringify(result, null, 2));
+          resolve(result);
+      }).catch(err => {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+          reject(err);
+      });
+
+  });
     
 }
 
 function putData(tableName, data){
+    //check
+    let inputData = fixEmptyValue(data);    
 
     let updateAttr = {};
     let updateExp = "set ";
     let replacedName = {};
     let num = 0;
-    for(let i in data){
+    for(let i in inputData){
         if(i == 'id')
             continue;
         updateExp += "#b" + num + "=" + ":a" + num +",";
         replacedName["#b"+num] = i;
-        updateAttr[":a"+num] = data[i];
+        updateAttr[":a"+num] = inputData[i];
         num++;
     }
     updateExp = updateExp.slice(0, updateExp.length-1); //remove last char
@@ -136,7 +165,7 @@ function putData(tableName, data){
     var params = {
         TableName: tableName,
         Key:{
-            "id": data.id
+            "id": inputData.id
         },
         UpdateExpression: updateExp,
         ExpressionAttributeNames: replacedName,
@@ -147,9 +176,9 @@ function putData(tableName, data){
     return new Promise((resolve, reject) => {
 
         docClient.update(params).promise().then(result => {
-            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+            console.log("UpdateItem succeeded:", JSON.stringify(inputData, null, 2));
              let outputData = result.Attributes;
-             outputData.id = data.id;
+             outputData.id = inputData.id;
             resolve(outputData);
         }).catch(err => {
             console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
@@ -185,6 +214,46 @@ function deleteData(tableName, data){
     
 }
 
+async function unittest(){
+  let table = "photo_tmp";
+  const dateTime = Date.now();
+  const timestamp = Math.floor(dateTime);
+
+  let runTest = async (testCase) => {
+    let output = await postData(table, testCase);
+    console.log(output);
+    let queryoutput = await queryDataById(table, testCase.id);
+    console.log(queryoutput);
+    testCase["newtest"] = ">///<";
+    let putoutput = await putData(table, testCase);
+    console.log(putoutput);
+    queryoutput = await queryDataById(table, testCase.id);
+    console.log(queryoutput);
+    let deleteoutput = await deleteData(table, testCase);
+    console.log(deleteoutput); 
+  }
+  
+  try{
+    let testData = {
+      "id": "r12345678-0s897661-1i"+timestamp,
+      "test1": "",
+      "test2": "1",
+      "test3": 0,
+      "test4": {
+        "test4-1": "1",
+        "test4-2": "",
+        "test4-3": 1
+      },
+      "test5": ["0", "", 0, "1", 1]
+    }
+    await runTest(testData);
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
+
 exports.queryById = queryDataById;
 exports.queryDataByName = queryDataByName;
 exports.scan = scanData;
@@ -193,3 +262,5 @@ exports.put = putData;
 exports.delete = deleteData;
 
 exports.scanDataByFilter = scanDataByFilter;
+
+exports.unittest = unittest;
